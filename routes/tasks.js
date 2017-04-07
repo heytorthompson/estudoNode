e3363e5d9b004607f27c5e627f98ccf9e00c91c1
Heytor <heytor.thompson@sodet.biz>
2017-04-07 17:35:38 -0300

    nvm rebuild mais implementacao do crud de routes/tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,64 @@
 module.exports = app => {
-const Tasks = app.db.models.Tasks;
-  app.get("/tasks", (req, res) => {
-    Tasks.findAll({}).then(tasks => {
-      res.json({tasks: tasks});
-    });
-  });
+	const Tasks = app.db.models.Tasks;
+
+	app.route("/tasks")
+		.all((req, res, next) => {
+			// Middleware de pré-execução das rotas
+			delete req.body.id;
+			next();
+		})
+		.get((req, res) => {
+			// "/tasks": Lista tarefas
+			Tasks.findAll({})
+				.then(result => res.json(result))
+				.catch(error => {
+				res.status(412).json({msg: error.message});
+			});
+		})
+		.post((req, res) => {
+			// "/tasks": Cadastra uma nova tarefa
+			Tasks.create(req.body)
+				.then(result => res.json(result))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		});
+	app.route("/tasks/:id")
+		.all((req, res) => {
+			// Middleware de pré-execução das rotas
+			delete req.body.id;
+			next();
+		})
+		.get((req, res) => {
+			// "/tasks/1": Consulta uma tarefa
+			Tasks.findOne({where: req.params})
+				.then(result => {
+					if(result){
+						res.json(result);
+					}else{
+						res.sendStatus(404);
+					}
+				})
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		})
+		.put((req, res) => {
+			// "/tasks/1": Atualiza uma tarefa
+			Tasks.update(req.body, {where: req.params})
+				.then(result => res.sendStatus(204))
+				.catch(error => {
+					res.status(412).json({msg: error.message});
+				});
+		})
+		.delete((req, res) => {
+			// "/tasks/1": Exclui uma tarefa
+			Tasks.destroy({where: req.params})
+				.then(result => res.sendStatus(204))
+				.catch(error => {
+					res.status(412).json({msg:error.message});
+				});
+		});	
+
+
 };
